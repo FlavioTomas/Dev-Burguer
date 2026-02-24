73bfa8e785b61b8ee08ef83166bb20cd0a4e2f62
Flavio <flavio.tomas.villa@gmail.com>
2026-02-24 10:19:54 -0300

    Criando um controller de usuarios e utilizando os metodos store/post para criar um usuario

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/user.js';
-import { v4 } from 'uuid';
+import UserController from './app/controllers/userController.js';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-	const user = {
-        id: v4(),
-		name: 'Flávio Tomás',
-		email: 'flavio@email.com',
-		password_hash: '1234',
-		admin: false,
-	};
-	await User.create(user);
-
-    res.status(201).json(user)
-});
+routes.post('/users', UserController.store);
 
 
 export default routes
