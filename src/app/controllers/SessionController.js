7a5d91e01e71d711593766cd78ff1dc88cb9733a
Flavio <flavio.tomas.villa@gmail.com>
2026-02-24 17:32:46 -0300

    Criando um SessionController para o login de usuarios

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,54 @@
+import bcrypt from 'bcrypt';
+import * as Yup from 'yup';
+import User from '../models/user.js';
+
+class SessionController {
+	async store(request, response) {
+		const schema = Yup.object({
+			email: Yup.string().email().required(),
+			password: Yup.string().min(6).required(),
+		});
+
+		const isValid = await schema.isValid(request.body, { strict: true });
+
+		const emailOrPasswordInvalid = () => {
+			return response
+				.status(400)
+				.json({ error: 'Email or password incorrect!' });
+		};
+
+		if (!isValid) {
+			emailOrPasswordInvalid();
+		}
+
+		const { email, password } = request.body;
+
+		const existingUser = await User.findOne({
+			where: {
+				email,
+			},
+		});
+
+		if (!existingUser) {
+			emailOrPasswordInvalid()
+		}
+
+		const isPasswordCorrect = await bcrypt.compare(
+			password,
+			existingUser.password_hash,
+		);
+
+		if (!isPasswordCorrect) {
+			emailOrPasswordInvalid()
+		}
+
+		return response.status(200).json({
+			id: existingUser.id,
+			name: existingUser.name,
+			email: existingUser.email,
+			admin: existingUser.admin,
+		});
+	}
+}
+
+export default new SessionController();
