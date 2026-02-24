09f0b488514681c7fe48adebe2aceece47550a6b
Flavio <flavio.tomas.villa@gmail.com>
2026-02-24 12:34:43 -0300

    Utilizando o Yup para fazer verificacao dos dados de cadastro do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,22 +7,34 @@
 */
 
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 import User from '../models/user.js';
 
 class UserController {
 	async store(request, response) {
+		const schema = Yup.object({
+			name: Yup.string().required(),
+			email: Yup.string().email().required(),
+			password_hash: Yup.string().min(6).required(),
+			admin: Yup.boolean(),
+		});
+
+		try {
+			schema.validateSync(request.body, { abortEarly: false, strict: true });
+		} catch (err) {
+			return response.status(400).json({ error: err.errors });
+		}
+
 		const { name, email, password_hash, admin } = request.body;
 
 		const existingUser = await User.findOne({
 			where: {
-				email
-			}
+				email,
+			},
 		});
 
 		if (existingUser) {
-			return response
-				.status(400)
-				.json({ message: 'Este email já está cadastrado!' });
+			return response.status(400).json({ message: 'Email already taken!' });
 		}
 
 		const user = await User.create({
