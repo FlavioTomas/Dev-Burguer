af14feac277c011325d9a0e7773aad1e3c5a37eb
Flavio <flavio.tomas.villa@gmail.com>
2026-02-26 16:31:46 -0300

    Corrigindo os controllers das rotas categories e criando verificacao se a category ja existe no bd

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController.js';
 import SessionController from './app/controllers/SessionController.js';
 import authMiddleware from './middlewares/auth.js';
 import ProductController from './app/controllers/ProductController.js';
+import CategoryController from './app/controllers/CategoryController.js';
 
 
 const routes = new Router();
@@ -19,8 +20,8 @@ routes.use(authMiddleware)
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
 
-routes.post('/categories', ProductController.store)
-routes.get('/categories', ProductController.index)
+routes.post('/categories', CategoryController.store)
+routes.get('/categories', CategoryController.index)
 
 
 
