7e0e14e0d9f58b703656d53db1565b3d19f2252b
Flavio <flavio.tomas.villa@gmail.com>
2026-02-28 13:43:15 -0300

    Criando a atualizazao dos produtos atraves do metodo update no ProductController

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -34,6 +34,51 @@ class ProductController {
 	}
 
 
+
+	async update(request, response) {
+		const schema = Yup.object({
+			name: Yup.string(),
+			price: Yup.number(),
+			category_id: Yup.number(),
+			offer: Yup.boolean()
+		});
+
+		try {
+			schema.validateSync(request.body, { abortEarly: false });
+		} catch (err) {
+			return response.status(400).json({ error: err.errors });
+		}
+
+		const { name, price, category_id, offer } = request.body;
+		const { id } = request.params
+
+		let path
+		if (request.file) {
+			const { filename } = request.file;
+			path = filename
+		}
+
+		await Product.update({
+			name,
+			price,
+			category_id,
+			path,
+			offer
+		}, {
+			where: {
+				id
+			}
+		})
+
+
+		return response.status(201).json();
+	}
+
+
+
+
+
+
 	async index(request, response) {
 		const products = await Product.findAll({
 			include: {
@@ -47,8 +92,6 @@ class ProductController {
 
 		return response.status(200).json(products)
 	}
-
-
 
 
 }
