8f35c6e2a99395db1d152fb049aeea85fad18e1b
Flavio <flavio.tomas.villa@gmail.com>
2026-02-28 20:31:33 -0300

    Criando banco de dados mongo, fazendo a conexao e criando o schema de pedidos

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import { Sequelize } from 'sequelize';
 import category from '../app/models/category.js';
 import product from '../app/models/product.js';
@@ -10,11 +11,17 @@ const models = [User, product, category];
 class Database {
 	constructor() {
 		this.init();
+		this.mongo()
 	}
 
 	init() {
 		this.connection = new Sequelize(databaseConfig);
 		models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models));
+	}
+
+
+	mongo(){
+		this.mongooseConnection = mongoose.connect('mongodb://localhost:27017/devburguer')
 	}
 }
 
