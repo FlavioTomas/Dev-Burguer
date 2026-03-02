780154218f275bdc72d794e751b5bf5a2162e80f
Flavio <flavio.tomas.villa@gmail.com>
2026-03-02 10:54:05 -0300

    Criando a rota do Login com o react router dom

--- Dev-burguer-frontend/devburguer-interface/src/main.jsx
+++ Dev-burguer-frontend/devburguer-interface/src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
+import { RouterProvider } from 'react-router-dom'
 
+import { router } from './routes'
 import GlobalStyles from './styles/globalStyles.js'
-import { Login } from './containers/Login'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <RouterProvider router={router}/>
     <GlobalStyles/>
     <ToastContainer autoClose={2000} theme='colored'/>
   </StrictMode>,
